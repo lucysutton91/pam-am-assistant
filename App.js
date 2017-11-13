f4ab40898df08b9fae63a21e54ba210cad689bcb
Lucy Sutton <Theodore@Lucys-Mac-mini.home>
2017-11-12 20:41:28 -0500

    working time picker with DatePicker component

--- App.js
+++ App.js
@@ -1,71 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text, Picker, StyleSheet } from 'react-native'
-import TimePicker from 'components/TimePicker';
+import TimePicker from './components/TimePicker';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      hour: '9',
-      minute: '30',
-      morningOrNight: 'am'
-    }
-    this.updateHour = this.updateHour.bind(this);
-    this.updateMinute = this.updateMinute.bind(this);
-    this.updateAmPm = this.updateAmPm.bind(this);
   }
-  updateHour(hour) {
-    this.setState({ hour: hour })
-  }
-  updateMinute(minute) {
-    this.setState({ minute: minute })
-  }
-  updateAmPm(morningOrNight) {
-    this.setState({ morningOrNight: morningOrNight })
-  }
+
   render() {
-    let hours = (function () {
-      let hrs = [];
-      for (let i = 1; i < 13; i++) {
-        var hour = i.toString();
-        hrs.push(hour)
-      }
-      return hrs
-    }());
-    let minutes = (function () {
-      let mins = ['00'];
-      for (let i = 1; i < 60; i++) {
-        let minute;
-        if (i < 10) minute = '0' + i.toString();
-        else minute = i.toString();
-        mins.push(minute)
-      }
-      return mins
-    }());
     return (
       <View style={styles.container}>
-        {
-        // <Picker style={styles.picker} selectedValue={this.state.hour} onValueChange={this.updateHour}>
-        //   {
-        //     hours.map(hour => {
-        //       return <Picker.Item key={hour} label={hour} value={hour} />
-        //     })
-        //   }
-        // </Picker>
-        // <Picker style={styles.picker} selectedValue={this.state.minute} onValueChange={this.updateMinute}>
-        //   {
-        //     minutes.map(minute => {
-        //       return <Picker.Item key={minute} label={minute} value={minute} />
-        //     })
-        //   }
-        // </Picker>
-        // <Picker style={styles.picker} selectedValue={this.state.morningOrNight} onValueChange={this.updateAmPm}>
-        //   <Picker.Item key="am" label="am" value="am" />
-        //   <Picker.Item key="pm" label="pm" value="pm" />        
-        // </Picker>
-      }
+        
         <TimePicker />
-        <Text style={styles.text}>{this.state.hour + ':' + this.state.minute + ' ' + this.state.morningOrNight}</Text>
       </View>
     )
   }
@@ -73,14 +19,11 @@ class App extends Component {
 export default App
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    alignSelf: 'center',
-    color: 'red'
-  },
+  
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between'
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   }
 })
--- components/TimePicker.js
+++ components/TimePicker.js
@@ -1,26 +1,88 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-native-datepicker'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default class MyDatePicker extends Component {
   constructor(props){
     super(props)
     this.state = {
-        time:"09:30"
+        time:"09:30",
+        changed: false
     }
   }
 
   render(){
     return (
-        <DatePicker
-            style={{width: 200}}
-            date={this.state.time}
-            mode="time"
-            format="HH:mm"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            minuteInterval={1}
-            onDateChange={(time) => {this.setState({time: time});}}
-        />
+        <View style={styles.container}>
+            <Text style={styles.text}>click to schedule wakeup</Text>
+            <DatePicker
+                style={styles.picker}
+                date={this.state.time}
+                mode="time"
+                format="HH:mm"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                minuteInterval={1}
+                showIcon={false}
+                customStyles={{
+                    dateInput: {
+                        borderRadius: 5
+                    }
+                }}
+                onDateChange={(time) => {this.setState({time: time, changed: true});}}
+            />
+            {
+                this.state.changed === true ?
+                <View style={styles.confirmed}>
+                    <Text>your alarm is set for {this.state.time}</Text>
+                </View>
+                :
+                <View style={styles.none}>
+                    <Text>no alarm scheduled</Text>
+                </View>
+            }
+            
+        </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 10
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF'
+    },
+    picker: {
+        width: 250,
+        margin: 10
+    },
+    confirmed: {
+        width: 250,
+        height: 40,
+        borderColor: '#7FF6CC',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        alignItems: 'center',
+        borderRadius: 5,
+        justifyContent: 'center'
+    }
+  });
+
+//   ,
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5
+//   },
